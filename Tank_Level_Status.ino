848c4da3fa67795118a2de62397fbf7e7ddac666
seldon57 <eulerc@gmail.com>
2016-07-31 21:25:09 -0500

    Incremental update

--- Tank_Level_Status.ino
+++ Tank_Level_Status.ino
@@ -1,4 +1,4 @@
-int TankLevelStatus(int level, float percent, float WaterLevel)
+int TankLevelStatus(int level, float percent, float distance)
 {
   int LowLow   = 1;
   int Low      = 2;
@@ -9,29 +9,29 @@ int TankLevelStatus(int level, float percent, float WaterLevel)
   //14.2875 mm equals 1 gallon
 
   // Tank level state change while filling
-  if ((level == LowLow) && (percent > 28.575))    // at 2 gallons
+  if ((level == LowLow) && (percent > 28.575/850*100))    // at 2 gallons
     level = Low;
-  if ((level == Low) && (percent > 85.725))       // at 6 gallons
+  if ((level == Low) && (percent > 85.725/850*100))       // at 6 gallons
     level = Normal;
-  if ((level == Normal) && (percent > 642.9375))  //at 45 gallons
+  if ((level == Normal) && (percent > 642.9375/850*100))  //at 45 gallons
     level = High;
-  if ((level == High) && (percent > 785.8125))    // at 55 gallons
+  if ((level == High) && (percent > 785.8125/850*100))    // at 55 gallons
     level = HighHigh;
 
   //Tank level state change while emptying
-  if ((level == HighHigh) && (percent < 714.375)) // at 50 gallons
+  if ((level == HighHigh) && (percent < 714.375/850*100)) // at 50 gallons
     level = High;
-  if ((level == High) && (percent < 628.65))      // at 44 gallons
+  if ((level == High) && (percent < 628.65/850*100))      // at 44 gallons
     level = Normal;
-  if ((level == Normal) && (percent < 71.4375))   // at 5 gallons
+  if ((level == Normal) && (percent < 71.4375/850*100))   // at 5 gallons
     level = Low;
-  if ((level == Low) && (percent < 14.2875))      // at 1 gallon
+  if ((level == Low) && (percent < 14.2875/850*100))      // at 1 gallon
     level = LowLow;
 
   //Tank level state change if HC-SR04 is not acting properly or fails
-  if (WaterLevel > 2000)      // HC-SR04 is directly up against what it is measuring
+  if (distance > 2000)      // HC-SR04 is directly up against what it is measuring
     level = HighHigh;
-  if (WaterLevel == 0)        // HC-SR04 has failed or is disconnected
+  if (distance == 0)        // HC-SR04 has failed or is disconnected
     level = HighHigh;
     
   return level;
