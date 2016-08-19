c68a6c2ac33756e510e57f1f9e7e8e0c4900225a
seldon57 <eulerc@gmail.com>
2016-08-18 21:57:27 -0500

    Changed pressure switch type
    Changed for normally closed pressure switch

--- Pressure_Switch_Read.ino
+++ Pressure_Switch_Read.ino
@@ -1,7 +1,7 @@
 bool PressureSwitchRead(int PressureSwitchPin)
 {
   
-  if(digitalRead(PressureSwitchPin) == LOW)
+  if(digitalRead(PressureSwitchPin) == HIGH) //HIGH for a normally closed switch
   { 
     delay(1000); // delay to debounce switch 
     return true; 
